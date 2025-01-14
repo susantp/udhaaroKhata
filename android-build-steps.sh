b64b15a1a8177e4efc52d7d14709a1fdc6fae926
Susant Paudel <techbizznepal@gmail.com>
2025-01-14 09:00:08 +0545

    refactor

--- android-build-steps.sh
+++
@@ -1,5 +0,0 @@
-npx expo prebuild --no-install --platform android && \
-npx expo export:embed --eager --platform android --dev false && \
-cd ./android && \
-./gradlew :app:assembleRelease && \
-cd ..
